a1865df9b8711d0299ef44089aebf08cbf9439af
amiribnjaman <amiribnjaman@gmail.com>
2025-05-08 22:36:32 +0600

    Choose us section design done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Components/Header";
 import ProImg from '../assets/img.jpg'
 import ProImg2 from '../assets/img2.jpg'
+import ChooseImg from '../assets/ch1.jpg'
 
 const Home = () => {
 
@@ -11,7 +12,7 @@ const Home = () => {
 
       {/* Banner section */}
       <div className="w-[90%] mx-auto">
-        <div className="bg-[#26A69A] mt-6 rounded items-center h-[80vh]">
+        <div className="bg-[#26A69A] mt-6 rounded-[15px] items-center h-[80vh]">
           <div className="w-[85%] mx-auto flex justify-between items-center">
             <div className="w-1/2 ">
               <h1 className="text-4xl font-semibold mt-10">
@@ -30,8 +31,16 @@ const Home = () => {
               </button>
             </div>
             <div className=" flex items-center justify-center pt-11">
-              <img src={ProImg} className="w-[220px] h-[380px] rounded-tr-[80px] rounded-tl-[80px] -mr-[50px] z-10 pt-6" alt="" />
-              <img src={ProImg2} className="w-[280px] h-[420px] rounded-tr-[100px] rounded-tl-[100px]" alt="" />
+              <img
+                src={ProImg}
+                className="w-[220px] h-[380px] rounded-tr-[80px] rounded-tl-[80px] -mr-[50px] z-10 pt-6"
+                alt=""
+              />
+              <img
+                src={ProImg2}
+                className="w-[280px] h-[420px] rounded-tr-[100px] rounded-tl-[100px]"
+                alt=""
+              />
               {/* <div>hello</div> */}
             </div>
           </div>
@@ -40,10 +49,9 @@ const Home = () => {
 
       {/* Why choose us Section */}
       <div className="my-12 w-[75%] mx-auto">
-        <div className="flex items-center h-[80vh]">
-          <div className="w-1/2 pr-10">
-            <img src="" alt="" />
-            <div>hello</div>
+        <div className="flex items-center  h-[80vh]">
+          <div className="w-1/2 pr-10 flex items-center">
+            <img src={ChooseImg} className="w-full h-full mr-10 rounded-[30px] opacity-80" alt="" />
           </div>
           <div className="w-1/2 pr-10">
             <h1 className="text-4xl font-semibold mt-10">
@@ -75,14 +83,49 @@ const Home = () => {
                 <strong className="text-gray-800">Fast Delivery:</strong> Get
                 your orders delivered quickly and efficiently to your doorstep.
               </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      {/* Feedback section */}
+      <div className="my-16 w-[75%] mx-auto">
+        <div className="flex items-center">
+          <div className="w-1/2 pr-10">
+            <h1 className="text-4xl font-semibold mt-10">
+              <span className="text-gray-800"> Happy Customer Feedback</span>
+            </h1>
+            <p className="text-gray-600 mt-4">
+              We value our customers' feedback and strive to improve our
+              services based on your suggestions. Your satisfaction is our top
+              priority.
+            </p>
+            <ul className=" pl-5 mt-4 text-gray-600">
               <li className="mb-2">
-                <strong className="text-gray-800">
-                  Excellent Customer Support:
-                </strong>{" "}
-                Our team is here to assist you with any queries or concerns.
+                "I had a great shopping experience! The delivery was fast and
+                the product quality is excellent."
+                <strong className="text-gray-800 ml-2">Jane Doy</strong>
+              </li>
+              <li className="mb-2">
+                "I love the variety of products available. I found everything I
+                needed in one place."{" "}
+                <strong className="text-gray-800 ml-2">Jane Smith</strong>
+              </li>
+              <li className="mb-2">
+                "The customer support team was very helpful and resolved my
+                issue quickly."{" "}
+                <strong className="text-gray-800 ml-2">Michael Brown</strong>
+              </li>
+              <li className="mb-2">
+                "I appreciate the secure payment options. I felt safe shopping
+                on this platform."
+                <strong className="text-gray-800 ml-2">Emily Johnson</strong>
               </li>
             </ul>
           </div>
+
+          {/* Comments */}
+          <div></div>
         </div>
       </div>
     </>
